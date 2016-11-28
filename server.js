e9905d38f8e5c26efd5d40b5add1e221bf6b4bb5
Sabin Payyappat <sabinpayyappat1994@gmail.com>
2016-11-28 21:54:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ var crypto = require('crypto');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
+
+<html lang="en">
+<head>
+<title>My Profile</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="keywords" content="My Profile Sabin Payyappat" />
+
+<link href="/ui/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+<link href="/ui/cobox.css" rel="stylesheet" type="text/css">
+<link href="/ui/portfolio.css" rel="stylesheet" type="text/css" media="all">
+<link href="/ui/style.css" rel="stylesheet" type="text/css" media="all">
+
+<link href='//fonts.googleapis.com/css?family=Quicksand:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Cinzel:400,700,900' rel='stylesheet' type='text/css'>
+	
+<script src="/ui/modernizr.custom.js"></script>
+
+</head>
+
+</html>
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
